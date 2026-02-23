78c2e785961d522599fc46eb3bd4f8f71e0b7b28
Roberto Villarreal <roberto.cesar136@hotmail.com>
2026-02-23 17:16:30 -0600

    feat: Extract node generation logic to the `trinity_generator` package and remove internal build dependencies.

--- lib/src/generator/builder.dart
+++
@@ -1,6 +0,0 @@
-import 'package:build/build.dart';
-import 'package:source_gen/source_gen.dart';
-import 'node_generator.dart';
-
-Builder nodeBuilder(BuilderOptions options) =>
-    LibraryBuilder(NodeGenerator(), generatedExtension: '.g.dart');
